4351080be4f90fec93a218871619940f84038349
Пользователь <artemkinartem6@gmail.com>
2022-08-04 12:36:54 +0300

    init: add Background images

--- momentum/index.js
+++ momentum/index.js
@@ -45,20 +45,81 @@ document.getElementById("greeting").innerHTML = time;
 
 //----------------Day time end-------------//
 //----------------Input--------------------//
-document.addEventListener("DOMContentLoaded", function() { // событие загрузки страницы
+var field = document.getElementById("name_");
 
-    // выбираем на странице все элементы типа textarea и input
-    document.querySelectorAll('textarea, input').forEach(function(e) {
-        // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
-        // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
-        if(e.value === '') e.value = window.sessionStorage.getItem(e.name, e.value);
-        // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
-        e.addEventListener('input', function() {
-            // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
-            window.sessionStorage.setItem(e.name, e.value);
-        })
-    })
+// Проверяем наличие значения 'autosave'
+// (это может произойти только если страница будет случайно обновлена)
+if (sessionStorage.getItem("autosave")) {
+  // Восстанавливаем содержимое текстового поля
+  field.value = sessionStorage.getItem("autosave");
+}
 
-}); 
+// Отслеживаем все изменения в текстовом поле
+field.addEventListener("change", function() {
+  // И сохраняем их в объект session storage
+  sessionStorage.setItem("autosave", field.value);
+});
 
 //----------------Input end----------------//
+
+//----------------Background images---------//
+
+var today = new Date();
+var hourNow = today.getHours();
+
+var style;
+var block = document.getElementById('body_');
+
+var timeOfDay;            
+
+if(hourNow >= 18) {
+    timeOfDay = 1;
+} else if (hourNow >= 12) {
+    timeOfDay = 2;
+} else if (hourNow > 0) {
+    timeOfDay = 3;
+} else {
+    timeOfDay = 0;
+}
+
+// Randomize 3 images            
+var rnd = Math.floor(Math.random() * (4 - 1) + 1); // Returns a random number between 1 (inclusive) and 4 (exclusive)
+
+switch (timeOfDay) {
+    case 1: // Evening
+        switch (rnd) {
+            case 1: block.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')"; break;
+            case 2: block.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/19.jpg')"; break;
+            case 3: block.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/20.jpg')"; break;
+        } 
+        break;
+    case 2: // Afternoon
+      
+        switch (rnd) {
+            case 1: block.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')"; break;
+            case 2: block.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/19.jpg')"; break;
+            case 3: block.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/20.jpg')"; break;
+
+            
+        } 
+        break;
+    case 0: // Morning
+
+        switch (rnd) {
+            case 1: document.write('<img src="MorningImage1.jpg" />'); break;
+            case 2: document.write('<img src="MorningImage2.jpg" />'); break;
+            case 3: document.write('<img src="MorningImage1.jpg" />'); break;
+        } 
+        break;
+    default: // Generic
+        
+        switch (rnd) {
+            case 1: document.write('<img src="GenericImage1.jpg" />'); break;
+            case 2: document.write('<img src="GenericImage2.jpg" />'); break;
+            case 3: document.write('<img src="GenericImage1.jpg" />'); break;
+        }
+        break;
+} 
+
+
+//---------------------------- Background images end------------------------//
