57bb68f9af4504d79a63ad23d4143f38571cfd59
Пользователь <artemkinartem6@gmail.com>
2022-08-08 22:57:23 +0300

    init: add audio

--- momentum/audio.js
+++ momentum/audio.js
@@ -1,15 +1,20 @@
+import playList from './playList.js';
 
-function playAudio(){
-if(isPlay=true){
-  audio.src = " ";
+const audio = new Audio();
+
+function playAudio() {
+  let playNum = 0;
+  audio.src = playList[playNum].src;// ссылка на аудио-файл;
   audio.currentTime = 0;
   audio.play();
-
-} else {
-  audio.pause();
 }
 
-}
+
 document.getElementById('play-id').addEventListener('click', ()=>{
  playAudio();
 });
+
+function toggleBtn() {
+  document.getElementById('play-id').classList.toggle('pause');
+}
+document.getElementById('play-id').addEventListener('click', toggleBtn);
---
+++ momentum/playList.js
@@ -0,0 +1,13 @@
+const playList = [
+    {      
+      title: 'Aqua Caelestis',
+      src: 'http://physical-authority.surge.sh/music/2.mp3',
+      duration: '00:58'
+    },  
+    {      
+      title: 'River Flows In You',
+      src: 'http://physical-authority.surge.sh/music/2.mp3',
+      duration: '03:50'
+    }
+  ]
+  export default playList;
